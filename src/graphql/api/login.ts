ac757d643d8dc9809252c02ced61d7faa3ebb6fd
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-05-20 21:43:43 +0900

    works

--- src/graphql/api/login.ts
+++ src/graphql/api/login.ts
@@ -8,11 +8,8 @@ export default async function loginApi(obj: {
   name: string;
   password: string;
 }) {
-  let result: any;
-
   try {
-    const query = JSON.stringify({
-      query: `
+    const query = `
     query getUser {
       getUser(input: { name: "${obj.name}", password: "${obj.password}" }) {
         token
@@ -20,19 +17,27 @@ export default async function loginApi(obj: {
         name
       }
     }
-  `
-    });
-  } catch (err) {
-    throw err;
-  }
-
-  if (result.getUser) {
-    const user = result.getUser as User;
-    return {
-      token: user.token,
-      code: user.code,
-      name: user.name
+  `;
+    const url = "https://echosome.tk:1333/query";
+    const opts = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query })
     };
+    const res = await fetch(url, opts);
+    const result = await res.json();
+    const data = result.data;
+
+    if (data.getUser) {
+      const user = data.getUser as User;
+      return {
+        token: user.token,
+        code: user.code,
+        name: user.name
+      };
+    }
+  } catch (err) {
+    console.error(err);
   }
 
   return undefined;
